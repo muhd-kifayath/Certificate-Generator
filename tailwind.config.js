26dec12d5e8791e5b0a45ca52326b8ee3060e29a
B Mohnish Karthikeyan <94187409+MohnishKarthikeyan@users.noreply.github.com>
2023-05-15 00:48:29 +0530

    adding the json files

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+  mode: "jit",
+  content: ["./Templates/*.html"],
+  theme: {
+    extend: {
+      // that is animation class
+      animation: {
+        fade: "fadeOut 5s ease-in-out",
+      },
+
+      // that is actual animation
+      keyframes: (theme) => ({
+        fadeOut: {
+          "0%": { backgroundColor: theme("colors.red.300") },
+          "100%": { backgroundColor: theme("colors.transparent") },
+        },
+      }),
+    },
+  },
+  variants: {},
+  plugins: [],
+};
