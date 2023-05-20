cd45abe8d846c59fe8299db092d38e3b32d193af
KnotSarvajit <sarvajitvisagan@gmail.com>
2023-05-21 00:55:00 +0530

    added dashboard and landing page

--- app.py
+++ app.py
@@ -14,5 +14,13 @@ def login_page():
 def signup():
     return render_template('signup.html')
 
+@app.route('/landing/')
+def landing():
+    return render_template('landing.html')
+
+@app.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard.html')
+
 if __name__ == "__main__":
     app.run(debug=True )   
